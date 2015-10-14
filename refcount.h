907aff20b3789e8fbee8f921c18e115225881549
justinkadima <justinkadima@gmail.com>
2015-10-14 23:01:39 +0300

    functions rename

--- refcount.h
+++ refcount.h
@@ -1,26 +1,19 @@
-//
-//  refcount.h
-//  glututor1
-//
-//  Created by Macbook White on 11/19/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
-//
 
 #ifndef REFCOUNT_H
 #define REFCOUNT_H
 
 #include<stdlib.h>
 
-typedef struct _refObject
+typedef struct
 {
     void* ptr;
     unsigned int retainCount;
 }RefObject;
 
 
-void* Alloc(size_t sz);
-void* Retain(void *obj);
-void Release(void *item);
-unsigned int getRetainCount(void *item);
+void* RC_Alloc(size_t sz);
+void* RC_Retain(void *obj);
+void RC_Release(void *item);
+unsigned int RC_GetRetainCount(void *item);
 
 #endif
