3b745d5d887ee67637fbc34524850525b3c6f05e
justin kadima <justinkadima@gmail.com>
2018-01-19 22:13:45 +0200

    update

--- debug.h
+++ debug.h
@@ -1,5 +1,3 @@
-
-
 #ifndef DEBUG_H
 #define DEBUG_H
 
@@ -17,14 +15,20 @@ typedef enum
     
 }DebugLevel;
 
+void __debug(char* file,int line,DebugLevel level,char *mes,...);
 
-#define __info(mes,...) debug(__FILE__,__LINE__,Info,mes,__VA_ARGS__)
+#define log_last_error() fprintf(stderr,"LAST ERROR: %s \n",strerror(errno))
 
-#define __fatal(mes,...) debug(__FILE__,__LINE__,Fatal,mes,__VA_ARGS__)
+#define log_info(mes,...) __debug(__FILE__,__LINE__,Info,mes,__VA_ARGS__)
 
-#define __warn(mes,...) debug(__FILE__,__LINE__,Warning,mes,__VA_ARGS__)
+#define log_fatal(mes,...) __debug(__FILE__,__LINE__,Fatal,mes,__VA_ARGS__)
 
-void debug(char* file,int line,DebugLevel level,char *mes,...);
+#define log_warning(mes,...) __debug(__FILE__,__LINE__,Warning,mes,__VA_ARGS__)
+
+
+int __check(char* file,int line,int condition);
+#define check(condition)  __check(__FILE__,__LINE__,condition)
+
 
 
 #endif
